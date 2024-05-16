7581b855af57745d9fc04e0844a3ee27b94104cb
Benjamin Carías <cariasbenjamin59@gmail.com>
2024-05-16 07:50:46 -0600

    Add Helmet Middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 import { taskRouter } from './routes/tasks.routes.js';
 import { loginRouter } from './routes/auth.routes.js';
@@ -19,6 +20,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(helmet());
 
 // Routes
 app.get('/', async (req, res) => {
