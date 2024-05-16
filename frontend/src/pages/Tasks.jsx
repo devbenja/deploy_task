254251c83414f3b1d7113d6011fb717e5852d4e6
Benjamin Carías <cariasbenjamin59@gmail.com>
2024-05-16 05:49:33 -0600

    Change loading for user state

--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -2,11 +2,14 @@ import { useEffect } from "react";
 import { TaskCard } from "../components/tasks/TaskCard";
 import { useTasks } from "../context/TaskContext";
 import { useAuth } from "../context/AuthContext";
+import { Container } from "../components/ui";
+import { ClipLoader } from "react-spinners";
 
 export const Tasks = () => {
 
   const { tasks, loadTasks } = useTasks();
- 
+  const { user } = useAuth();
+
   useEffect(() => {
     loadTasks();
   }, []);
@@ -19,10 +22,19 @@ export const Tasks = () => {
   )
 
   return (
-    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 mx-10 mt-5">
-      {tasks.map((task) => (
-        <TaskCard task={task} key={task.id_task} />
-      ))}
-    </div>
+    <Container>
+      {user ? (
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 mx-10 mt-5">
+          {tasks.map((task) => (
+            <TaskCard task={task} key={task.id_task} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex justify-center max-w-xs p-6 shadow-md rounded-xl sm:px-12 bg-gray-900 text-gray-100">
+          <ClipLoader color="blue" size={50} />
+        </div>
+      )
+      }
+    </Container>
   );
 }
